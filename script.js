2bc99f4bf17bbf647f1527b0bdde124f8370d90f
VIVEK PAL <142665166+Vivekpal756216@users.noreply.github.com>
2025-10-04 23:41:13 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function copyCode() {
-  let code = document.querySelector("#codeBox code").innerText;
-  navigator.clipboard.writeText(code).then(() => {
-    alert("✅ Code copied to clipboard!");
-  });
-}
